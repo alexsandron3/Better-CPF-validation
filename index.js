da6d6c665a2ee5006bfccb535cd2a61abdb311b7
Alexsandro Xavier da Silveira <62912497+alexsandron3@users.noreply.github.com>
2022-02-02 03:57:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/', (req, res) => {
       // Browser config
       const browser = await puppeteer.launch({
         headless: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--headless'],
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
       });
       const page = await browser.newPage();
 
