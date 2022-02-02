99dbf0d6368ecaa7d94fb109d188e85028bb6f80
Alexsandro Xavier da Silveira <62912497+alexsandron3@users.noreply.github.com>
2022-02-02 02:28:57 -0300

    Create express server

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-// Script initial setup;
+// Libs
 const puppeteer = require('puppeteer');
 require('dotenv').config();
+const express = require('express'); 
+const app = express(); 
 
-const END_POINT = 'https://servicos.receita.fazenda.gov.br/Servicos/CPF/ConsultaSituacao/ConsultaPublica.asp';
+// Script initial setup;
+const END_POINT =
+  'https://servicos.receita.fazenda.gov.br/Servicos/CPF/ConsultaSituacao/ConsultaPublica.asp';
 
 const timeout = 180000; // in milliseconds
 
 const CPF = process.env.CPF;
 const BIRTH_DAY = process.env.BIRTH_DAY;
+const PORT = process.env.PORT || 7000;
 
-const IS_CAPTCHA_FILLED = 'document.querySelector("[data-hcaptcha-response]").dataset.hcaptchaResponse !== ""';
-const USER_SITUATION_SELECTOR = '#mainComp > div:nth-child(3) > p > span:nth-child(10)';
-  (
-    async () => {
+const IS_CAPTCHA_FILLED =
+  'document.querySelector("[data-hcaptcha-response]").dataset.hcaptchaResponse !== ""';
+const USER_SITUATION_SELECTOR =
+  '#mainComp > div:nth-child(3) > p > span:nth-child(10)';
 
+app.get('/', (req, res) => {
+  const { query } = req;
+  res.send(query.cpf);
+  const main = async () => {
+    try {
       // Browser config
       const browser = await puppeteer.launch({
         headless: false,
@@ -29,10 +39,7 @@ const USER_SITUATION_SELECTOR = '#mainComp > div:nth-child(3) > p > span:nth-chi
       // Send data
       const submitData = await page.$('#id_submit');
       await submitData.click();
-      await page.waitForSelector(
-        USER_SITUATION_SELECTOR,
-        { timeout },
-      );
+      await page.waitForSelector(USER_SITUATION_SELECTOR, { timeout });
       // Verify user situtation
       const elementUserSituation = await page.$(USER_SITUATION_SELECTOR);
       const textUserSituation = await page.evaluate(
@@ -40,7 +47,16 @@ const USER_SITUATION_SELECTOR = '#mainComp > div:nth-child(3) > p > span:nth-chi
         elementUserSituation,
       );
       const isUserRegular = textUserSituation === 'Situação Cadastral: REGULAR';
-      console.log(isUserRegular);
       await browser.close();
-    },
-  )();
+      res.send(isUserRegular);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  main();
+});
+
+// Making Express listen on port 7000
+app.listen(PORT, function () {
+  console.log(`Running on port 7000.`);
+});
