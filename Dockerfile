e0999c5a3ddd073e89259d1aef134231d958d7b9
Alexsandro Xavier da Silveira <62912497+alexsandron3@users.noreply.github.com>
2022-02-02 16:10:17 -0300

    refact code to work with headless

---
+++ Dockerfile
@@ -0,0 +1,39 @@
+FROM alpine
+
+# EXPOSE
+
+EXPOSE 7000
+
+# Installs latest Chromium (92) package.
+RUN apk add --no-cache \
+  chromium \
+  nss \
+  freetype \
+  harfbuzz \
+  ca-certificates \
+  ttf-freefont \
+  nodejs \
+  npm \
+  xvfb
+# Tell Puppeteer to skip installing Chrome. We'll be using the installed package.
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
+  PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser
+
+# Puppeteer v10.0.0 works with Chromium 92.
+
+# COPY FILES
+
+COPY ./package.json /
+COPY ./index.js /
+
+RUN npm install
+
+
+# Add user so we don't need --no-sandbox.
+RUN addgroup -S pptruser && adduser -S -g pptruser pptruser \
+  && mkdir -p /home/pptruser/Downloads /app \
+  && chown -R pptruser:pptruser /home/pptruser \
+  && chown -R pptruser:pptruser /app
+
+# Run everything after as non-privileged user.
+USER pptruser
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const chromium = require('chrome-aws-lambda');
+const axios = require('axios');
 
 app.use(express.json());
 
@@ -11,23 +11,18 @@ app.use(express.json());
 const END_POINT =
   'https://servicos.receita.fazenda.gov.br/Servicos/CPF/ConsultaSituacao/ConsultaPublica.asp';
 
+const CAPTCHA_VALIDATION_END_POINT = 'https://api.anycaptcha.com';
 const timeout = 180000; // in milliseconds
-
-/*
-  if you are using environment variables 
-  const CPF = process.env.CPF;
-  const BIRTH_DAY = process.env.BIRTH_DAY;
-*/
 const PORT = process.env.PORT || 7000;
+const SECRET = process.env.SECRET;
 
 const IS_CAPTCHA_FILLED =
   'document.querySelector("[data-hcaptcha-response]").dataset.hcaptchaResponse !== ""';
 const USER_SITUATION_SELECTOR =
   '#mainComp > div:nth-child(3) > p > span:nth-child(10)';
-
 // Express server
 app.listen(PORT, function () {
-  console.log(`Running on port 7000.`);
+  console.log(`Running on port ${PORT}`);
 });
 
 app.post('/', (req, res) => {
@@ -35,15 +30,11 @@ app.post('/', (req, res) => {
   const main = async () => {
     try {
       // Browser config
-      const browser = await chromium.puppeteer.launch({
-        headless: false,
-        args: [
-          '--no-sandbox',
-        //   '--disable-setuid-sandbox',
-        //   '--disable-dev-shm-usage',
-        ],
+      const browser = await puppeteer.launch({
+        headless: true,
       });
       const page = await browser.newPage();
+      page.setViewport({ width: 800, height: 600 });
       await page.setExtraHTTPHeaders({
         'Accept-Language': 'en-US,en;q=0.9',
       });
@@ -51,24 +42,78 @@ app.post('/', (req, res) => {
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
       );
       // Fill form
+      console.log('Step 1: Fill form with POST data');
       await page.goto(END_POINT);
       await page.type('#txtCPF', CPF);
       await page.type('#txtDataNascimento', BIRTH_DAY);
-      await page.waitForFunction(IS_CAPTCHA_FILLED, { timeout });
 
-      // Send data
-      const submitData = await page.$('#id_submit');
-      await submitData.click();
-      await page.waitForSelector(USER_SITUATION_SELECTOR, { timeout });
-      // Verify user situtation
-      const elementUserSituation = await page.$(USER_SITUATION_SELECTOR);
-      const textUserSituation = await page.evaluate(
-        (element) => element.textContent,
-        elementUserSituation,
+      // Fill captcha
+      console.log('Step 2: Fill captcha');
+      await page.waitForSelector('#id_captchasonoro');
+      const buttonCptToText = await page.$('#id_captchasonoro');
+      await buttonCptToText.click();
+      await page.waitForSelector('#imgCaptcha');
+      const imgCaptcha = await page.$('#imgCaptcha');
+      const imgSrc = await page.evaluate(
+        (imgCaptcha) => imgCaptcha.getAttribute('src'),
+        imgCaptcha,
       );
-      const isUserRegular = textUserSituation === 'Situação Cadastral: REGULAR';
-      await browser.close();
-      res.send({ isUserRegular });
+      const base64Img = imgSrc.split('base64,').pop();
+      const requestBody = {
+        clientKey: SECRET,
+        task: {
+          type: 'ImageToTextTask',
+          body: base64Img,
+        },
+      };
+      const {
+        data: { taskId },
+      } = await axios.post(`${CAPTCHA_VALIDATION_END_POINT}/createTask`, {
+        ...requestBody,
+      });
+
+      const get = async () => {
+        console.log('Trying to resolve CAPTCHA');
+        const { data } = await axios.post(
+          `${CAPTCHA_VALIDATION_END_POINT}/getTaskResult`,
+          {
+            clientKey: SECRET,
+            taskId,
+          },
+        );
+        try {
+          if (!data.solution) {
+            setTimeout(() => {
+              get();
+            }, 10000);
+          } else {
+            const { text } = data.solution;
+            page.type('#txtTexto_captcha_serpro_gov_br', text);
+            await page.waitForTimeout(1000);
+            // Send data
+            console.log('Step 3: Sending data do RF');
+            const submitData = await page.$('#id_submit');
+            await submitData.click();
+            await page.waitForSelector(USER_SITUATION_SELECTOR, { timeout });
+            // Verify user situtation
+            console.log('Step 4: Verify user situtation');
+            const elementUserSituation = await page.$(USER_SITUATION_SELECTOR);
+            const textUserSituation = await page.evaluate(
+              (element) => element.textContent,
+              elementUserSituation,
+            );
+            const isUserRegular =
+              textUserSituation === 'Situação Cadastral: REGULAR';
+
+            res.send({ isUserRegular });
+            console.log({ isUserRegular });
+            await browser.close();
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      get();
     } catch (error) {
       console.error(error);
     }
