a48d6a3ba672543e8cc6500a2d8d6eec32dd365b
Alexsandro Xavier da Silveira <62912497+alexsandron3@users.noreply.github.com>
2022-02-02 05:06:16 -0300

    teste

--- README.md
+++
@@ -1,5 +0,0 @@
-Como utilizar este script
-
-1 - Rode npm install OU yarn install
-2 - Crie um arquivo .env e configure suas variaveis de ambiente OU substitua as constantes pelo valor que você deseja;
-3 - Rode node index.js
--- index.js
+++ index.js
@@ -1,77 +1,15 @@
-// Libs
-const puppeteer = require('puppeteer');
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const chromium = require('chrome-aws-lambda');
+const puppeteer = require("puppeteer");
 
-app.use(express.json());
+async function main() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox"]
+  });
+  const tab = await browser.newPage();
+  const text = await (await tab.goto("http://example.com/")).text();
+  console.log(text);
+  console.log("done");
+  browser.close();
+}
 
-// Script initial setup;
-const END_POINT =
-  'https://servicos.receita.fazenda.gov.br/Servicos/CPF/ConsultaSituacao/ConsultaPublica.asp';
-
-const timeout = 180000; // in milliseconds
-
-/*
-  if you are using environment variables 
-  const CPF = process.env.CPF;
-  const BIRTH_DAY = process.env.BIRTH_DAY;
-*/
-const PORT = process.env.PORT || 7000;
-
-const IS_CAPTCHA_FILLED =
-  'document.querySelector("[data-hcaptcha-response]").dataset.hcaptchaResponse !== ""';
-const USER_SITUATION_SELECTOR =
-  '#mainComp > div:nth-child(3) > p > span:nth-child(10)';
-
-// Express server
-app.listen(PORT, function () {
-  console.log(`Running on port 7000.`);
-});
-
-app.post('/', (req, res) => {
-  const { CPF, BIRTH_DAY } = req.body;
-  const main = async () => {
-    try {
-      // Browser config
-      const browser = await chromium.puppeteer.launch({
-        headless: false,
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-dev-shm-usage',
-        ],
-      });
-      const page = await browser.newPage();
-      await page.setExtraHTTPHeaders({
-        'Accept-Language': 'en-US,en;q=0.9',
-      });
-      await page.setUserAgent(
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
-      );
-      // Fill form
-      await page.goto(END_POINT);
-      await page.type('#txtCPF', CPF);
-      await page.type('#txtDataNascimento', BIRTH_DAY);
-      await page.waitForFunction(IS_CAPTCHA_FILLED, { timeout });
-
-      // Send data
-      const submitData = await page.$('#id_submit');
-      await submitData.click();
-      await page.waitForSelector(USER_SITUATION_SELECTOR, { timeout });
-      // Verify user situtation
-      const elementUserSituation = await page.$(USER_SITUATION_SELECTOR);
-      const textUserSituation = await page.evaluate(
-        (element) => element.textContent,
-        elementUserSituation,
-      );
-      const isUserRegular = textUserSituation === 'Situação Cadastral: REGULAR';
-      await browser.close();
-      res.send({ isUserRegular });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  main();
-});
+main();
